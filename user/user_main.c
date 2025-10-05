e338416ea711801cf415ff67856a0243c70cd867
plumbum <1990093466@qq.com>
2025-10-05 14:52:28 +0800

    pid

--- user/user_main.c
+++ user/user_main.c
@@ -15,26 +15,26 @@ void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs)
 		HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0, &rxcan_3.rx_header, rxcan_3.data);
 	}
 
-
-    data_extract(&wheel,&rxcan_3);
+	data_extract(&wheel, &rxcan_3);
 }
 void tim_init(void)
 {
 	__HAL_TIM_SET_PRESCALER(&htim1, 0);
-	__HAL_TIM_SET_AUTORELOAD(&htim1, SYS_CLK/MAIN_FREQ - 1);
+	__HAL_TIM_SET_AUTORELOAD(&htim1, SYS_CLK / MAIN_FREQ - 1);
 	__HAL_TIM_SET_COUNTER(&htim1, 0);
 	HAL_TIM_Base_Start_IT(&htim1);
-	__HAL_TIM_SET_PRESCALER(&htim8, SYS_CLK/1000000);
-	__HAL_TIM_SET_AUTORELOAD(&htim8, 1000000/MOTOR_CONTROL_FREQ - 1);
+	__HAL_TIM_SET_PRESCALER(&htim8, SYS_CLK / 1000000);
+	__HAL_TIM_SET_AUTORELOAD(&htim8, 1000000 / MOTOR_CONTROL_FREQ - 1);
 	__HAL_TIM_SET_COUNTER(&htim8, 0);
 	HAL_TIM_Base_Start_IT(&htim8);
 }
 void user_init(void)
 {
-	
+
 	can_init();
 	serial_init();
-	
+	pid_init(wheel.velocity_pid+2,0.1,0,0,1,5);
+	wheel.velocity_target[2]=10;
 	tim_init();
 }
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
@@ -43,10 +43,11 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	{
 		serial_voltage_output_test_dma(&transmit_packet);
 	}
-	else if( htim->Instance == TIM8)
+	else if (htim->Instance == TIM8)
 	{
-    current_adjust_all(&wheel,1.5);
-    //current_adjust(&wheel,2,1.5);
-		current_set(&wheel,&txcan_3);
+		// current_adjust_all(&wheel, 1.5);
+		//  current_adjust(&wheel,2,1.5);
+		all_velocity_loop_cal(&wheel);
+		current_set(&wheel, &txcan_3);
 	}
 }
