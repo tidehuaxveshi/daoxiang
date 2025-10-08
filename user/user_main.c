a1c4768ecfd5dd65399f9bbaf92529090e28545b
plumbum_laptop <1990093466@qq.com>
2025-10-09 01:28:53 +0800

    ww

--- user/user_main.c
+++ user/user_main.c
@@ -51,7 +51,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 		//rm_motor_control();
 		//BRT38_read_cmd(1);
 		//j60_group_control_set(&j60_group);
-		//j60_control_set(j60_group.j60+3, &j60_group.j60_tx);
-     comm_can_set_rpm( 74, 600);
+		j60_control_set(j60_group.j60+3, &j60_group.j60_tx);
+     //comm_can_set_rpm( 74, 600);
 	}
 }
